f0cdc2563628b221c1904ee809cdaceaf97fe5f9
Lolloc99 <lolloc99@yahoo.it>
2022-05-19 16:36:18 +0200

    Esercizio base completato

---
+++ src/components/AppCdList.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="card">
+        <img :src="cardObject.poster" :alt="cardObject.title">
+        <h3>{{cardObject.title}}</h3>
+        <p>{{cardObject.author}}</p>
+        <small>{{cardObject.year}}</small>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "AppCdList",
+    props: ["cardObject"]
+}
+</script>
+
+<style lang="scss" scoped>
+.card {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 1rem;
+    margin: .3rem 1rem;
+    width: calc(100% / 5 - 2rem);
+    text-align: center;
+    max-height: 340px;
+    min-width: 180px;
+    background-color: #2e3a46;
+
+    img {
+        width: 100%;
+    }
+
+    h3 {
+        margin: 1rem 0;
+        color: white;
+        text-transform: uppercase;
+    }
+
+    small {
+        font-size: 1rem;
+        color: grey;
+    }
+
+    p {
+        font-size: 1.3rem;
+        color: grey;
+    }
+}
+</style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,74 +1,36 @@
 <template>
     <main id="main">
         <div class="container">
-            <div class="card">
-                <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="prova">
-                <h3>New Jersey</h3>
-                <p>Bon Jovi</p>
-                <small>1988</small>
-            </div>
-            <div class="card">
-                <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="prova">
-                <h3>New Jersey</h3>
-                <p>Bon Jovi</p>
-                <small>1988</small>
-            </div>
-            <div class="card">
-                <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="prova">
-                <h3>New Jersey</h3>
-                <p>Bon Jovi</p>
-                <small>1988</small>
-            </div>
-            <div class="card">
-                <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="prova">
-                <h3>New Jersey</h3>
-                <p>Bon Jovi</p>
-                <small>1988</small>
-            </div>
-            <div class="card">
-                <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="prova">
-                <h3>New Jersey</h3>
-                <p>Bon Jovi</p>
-                <small>1988</small>
-            </div>
-            <div class="card">
-                <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="prova">
-                <h3>New Jersey</h3>
-                <p>Bon Jovi</p>
-                <small>1988</small>
-            </div>
-            <div class="card">
-                <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="prova">
-                <h3>New Jersey</h3>
-                <p>Bon Jovi</p>
-                <small>1988</small>
-            </div>
-            <div class="card">
-                <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="prova">
-                <h3>New Jersey</h3>
-                <p>Bon Jovi</p>
-                <small>1988</small>
-            </div>
-            <div class="card">
-                <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="prova">
-                <h3>New Jersey</h3>
-                <p>Bon Jovi</p>
-                <small>1988</small>
-            </div>
-            <div class="card">
-                <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="prova">
-                <h3>New Jersey</h3>
-                <p>Bon Jovi</p>
-                <small>1988</small>
-            </div>
+            <AppCdList 
+                v-for="(item, index) in cardContent"
+                :key="index"
+                :cardObject="item"
+            />
         </div>
     </main>
 </template>
 
 <script>
+import AppCdList from "./AppCdList";
+import axios from "axios";
 export default {
     name: "AppMain",
-}
+    components: {
+        AppCdList,
+    },
+    data: function() {
+        return {
+            cardContent: [],
+        };
+    },
+    created() {
+        axios.get("https://flynn.boolean.careers/exercises/api/array/music")
+        .then((resp) =>{
+            const cdArray = resp.data.response;
+            this.cardContent = cdArray;
+        });
+    }
+};
 </script>
 
 <style lang="scss" scoped>
@@ -85,40 +47,5 @@ export default {
     justify-content: flex-start;
     flex-wrap: wrap;
     width: 60%;
-    overflow-y: auto;
-}
-
-.card {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 1rem;
-    margin: 1rem;
-    width: calc(100% / 5 - 2rem);
-    text-align: center;
-    max-height: 340px;
-    min-width: 180px;
-    overflow-y: auto;
-    background-color: #2e3a46;
-
-    img {
-        width: 100%;
-    }
-
-    h3 {
-        margin: 1rem 0;
-        color: white;
-        text-transform: uppercase;
-    }
-
-    p,
-    small {
-        font-size: 1.2rem;
-        color: grey;
-    }
-
-    p {
-        font-size: 1.4rem;
-    }
 }
 </style>
